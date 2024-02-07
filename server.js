10254898f126edb82b685221004be84935bc8ff3
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-02-08 01:53:31 +0200

    server setup part 2

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-import express from "express";
-import dotenv from "dotenv";
-
+const express = require("express");
 const app = express();
-dotenv.config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-app.get("/", (req, res) => res.send("hello server"));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
 
+app.use("/api", require("./routes/authRoutes"));
+
+app.get("/", (req, res) => res.send("My backend"));
 const port = process.env.PORT;
-
-app.listen(port, () => console.log(`server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
