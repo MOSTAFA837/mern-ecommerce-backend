e73bd9d15c61d0eab0aae64d66682d545915c1ce
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-02-10 14:33:28 +0200

    seller register and login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
 
 const adminModel = require("../models/admin.model");
+const Seller = require("../models/seller.model");
+const SellerCustomer = require("../models/sellerCustomer.model");
 const { responseReturn } = require("../utils/response");
 const { createToken } = require("../utils/tokenCreate");
 
@@ -43,11 +45,71 @@ class authControllers {
       if (role === "admin") {
         const user = await adminModel.findById(id);
         responseReturn(res, 200, { userInfo: user });
-      } else {
-        console.log("Seller Info");
+      } else if (role === "seller") {
+        const seller = await Seller.findById(id);
+        responseReturn(res, 200, { userInfo: seller });
       }
     } catch (error) {
       console.log(error.message);
+    }
+  };
+
+  seller_register = async (req, res) => {
+    const { email, name, password } = req.body;
+
+    try {
+      const seller = await Seller.findOne({ email });
+
+      if (seller) {
+        responseReturn(res, 404, { error: "Email already exists" });
+      } else {
+        const seller = await Seller.create({
+          name,
+          email,
+          password: await bcrypt.hash(password, 10),
+        });
+
+        await SellerCustomer.create({
+          myId: seller._id,
+        });
+
+        const token = await createToken({ id: seller._id, role: seller.role });
+        res.cookie("accessToken", token, {
+          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+
+        responseReturn(res, 201, { token, message: "Register Success" });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: "Internal Server Error" });
+    }
+  };
+
+  seller_login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const seller = await Seller.findOne({ email }).select("+password");
+
+      if (seller) {
+        const match = await bcrypt.compare(password, seller.password);
+
+        if (match) {
+          const token = await createToken({
+            id: seller._id,
+            role: seller.role,
+          });
+
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "Login Success" });
+        } else {
+          responseReturn(res, 404, { error: "Password Wrong" });
+        }
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
     }
   };
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,17 @@
-const { admin_login, getUser } = require("../controllers/authControllers");
+const {
+  admin_login,
+  getUser,
+  seller_register,
+  seller_login,
+} = require("../controllers/authControllers");
 const { authMiddleware } = require("../middlewares/authMiddleware");
 
 const router = require("express").Router();
 
 router.post("/admin-login", admin_login);
 router.get("/get-user", authMiddleware, getUser);
+
+router.post("/seller-register", seller_register);
+router.post("/seller-login", seller_login);
 
 module.exports = router;
